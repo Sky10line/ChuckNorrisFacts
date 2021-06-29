a997176432e8333d583018bbf126395f2d73d662
Sky10line <61299872+Sky10line@users.noreply.github.com>
2021-06-29 18:25:52 -0300

    Project created
    - Project created
    - View code Configuration setted
    - Dependencies added

---
+++ ChuckNFacts/ChuckNFacts/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ChuckNFacts
+//
+//  Created by Rodrigo Ryo Aoki on 29/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+		let view = UIView()
+		view.backgroundColor = .systemBlue
+		self.view = view
+	}
+
+
+}
+
---
+++ ChuckNFacts/Pods/Target Support Files/Pods-ChuckNFacts/Pods-ChuckNFacts-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChuckNFacts : NSObject
+@end
+@implementation PodsDummy_Pods_ChuckNFacts
+@end
